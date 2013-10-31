c48fa28c982a7f0e871c350e1de0354dbfad8a57
Varya Stepanova <mail@varya.me>
2013-10-31 09:40:53 +0100

    Init blocks automatically

--- desktop.blocks/page/page.deps.js
+++ desktop.blocks/page/page.deps.js
@@ -1,6 +1,13 @@
 ({
     shouldDeps: [
         { block: 'grid' },
-        { block: 'text' }
+        { block: 'text' },
+        {
+            block: 'i-bem',
+            elem: 'dom',
+            mods: {
+                init: ['auto']
+            }
+        }
     ]
 })
