bf2a488a95727bf0f3565e4a221f4e6eafa8f969
Varvara Stepanova <toivonen@toivonen.ru>
2013-11-18 21:41:10 +0100

    Highlight every block separately

--- desktop.blocks/highlight/__init/highlight__init.js
+++ desktop.blocks/highlight/__init/highlight__init.js
@@ -1,8 +1,21 @@
 /** @requires BEM */
 /** @requires BEM.DOM */
 
-modules.require(['highlight'], function(hljs) {
+modules.require(['highlight', 'jquery'], function(hljs, $) {
 
-hljs.initHighlightingOnLoad();
+    var langAliases = {
+        js: 'javascript'
+    }
+
+    $(function(){
+        $('.highlight pre code').each(function(i, block){
+            var $block = $(block),
+                lang = $block.attr('data-lang');
+            $block.addClass(langAliases[lang] || lang);
+            hljs.highlightBlock(block);
+        })
+    })
+
+    //hljs.initHighlightingOnLoad();
 
 });
