7ccbbdc91a9295e2b7073e110c008fde4f722c7c
Varvara Stepanova <toivonen@toivonen.ru>
2013-11-19 21:25:36 +0100

    Redefine some highlight__init methods for the blog only

--- desktop.blocks/highlight/__init/highlight__init.js
+++ desktop.blocks/highlight/__init/highlight__init.js
@@ -1,35 +1,15 @@
 /** @requires BEM */
 /** @requires BEM.DOM */
 
-modules.define('i-bem__dom', ['highlight', 'jquery'], function(provide, hljs, $, DOM) {
-
-var langAliases = {
-    js: 'javascript',
-    html: 'xml'
-}
+modules.define('i-bem__dom', ['jquery'], function(provide, $, DOM) {
 
 DOM.decl('highlight', {
-    onSetMod: {
-        "js": {
-            "inited" : function() {
-                this._langHack();
-                hljs.highlightBlock(this.getCodeContainer());
-            }
-        }
-    },
-    _langHack: function() {
-        var lang = this.params.lang;
-        lang = langAliases[lang] || lang;
-
-        this.get$CodeContainer().addClass(lang);
-    },
     getCodeContainer: function() {
         this._codeContainer = this._codeContainer || this.domElem.find('pre code')[0];
         return this._codeContainer;
-        return this.elem('code');
     },
     get$CodeContainer: function() {
-        this._$CodeContainer = this._$CodeContainer || $(this.getCodeContainer())
+        this._$CodeContainer = this._$CodeContainer || $(this.getCodeContainer());
         return this._$CodeContainer;
     },
     getDefaultParams: function() {
