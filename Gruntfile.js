d156435b3b6fae1eaa2ba617e7dc37a5910b09b2
Varya Stepanova <mail@varya.me>
2014-05-24 21:49:32 +0400

    Copy CNAME

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,6 +26,11 @@ module.exports = function(grunt) {
                 files: {
                     "out/": ["data/*"]
                 }
+            },
+            cname: {
+                files: {
+                    "out/": ["./CNAME"]
+                }
             }
         }
     });
