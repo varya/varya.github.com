06f73d26b5363737850d7b2458a7542ab6a43146
Varya Stepanova <mail@varya.me>
2014-06-15 13:28:40 +0400

    Use ENB in Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,9 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        bem: {
-            options: {
-                require: './node_modules/bem',
-                root: './'
-            },
-            bundles: {
-                targets: 'desktop.bundles'
+        shell: {
+            enb: {
+                command: './node_modules/enb/bin/enb make'
             }
         },
         copy: {
@@ -36,6 +32,6 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-bem');
+    grunt.loadNpmTasks('grunt-shell');
 
 }
