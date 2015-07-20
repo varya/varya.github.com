517347660c736ff2a9ba522c6ab8963b167465f7
Varya Stepanova <mail@varya.me>
2015-07-20 16:34:24 +0300

    Dynamic tests on body

--- tests/gemini-test.js
+++ tests/gemini-test.js
@@ -1,7 +1,37 @@
 var gemini = require('gemini');
 
-gemini.suite('varya', function(suite) {
-    suite.setUrl('/')
-        .setCaptureElements('.social-ico')
-        .capture('plain');
+var examples = [
+  '1.1',
+  '1.2-1',
+  '2.1'
+];
+
+examples.forEach(function(example) {
+
+  gemini.suite(example, function(suite) {
+      suite.setUrl('/styleguide/#/section/' + example + '/fullscreen')
+          .setCaptureElements('body')
+          .capture('plain', function(actions, find){
+              /*actions.executeJS(function(window) {
+
+                  window.modules.require(
+                      ['i-bem__dom', 'jquery'],
+                      function(BEMDOM, $) {
+
+                      $(function() {
+                          $(window).bind("styleguide:onRendered", function(e) {
+                              window.doTake = true;
+                          });
+                      });
+
+                  });
+
+              })
+              actions.waitForJSCondition(function(window) {
+                  return window.doTake;
+              }, 2000);*/
+              actions.wait(3000)
+          });
+  });
+
 });
