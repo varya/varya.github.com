f07a106fb940a9915bbdb8df1d47cb80449fdb32
Varya Stepanova <mail@varya.me>
2015-07-23 15:06:23 +0300

    Take screenshots when rendered

--- tests/gemini-test.js
+++ tests/gemini-test.js
@@ -12,17 +12,16 @@ examples.forEach(function(example) {
       suite.setUrl('/styleguide/#/section/' + example + '/fullscreen')
           .setCaptureElements('body')
           .capture('plain', function(actions, find){
-              /*actions.executeJS(function(window) {
+              actions.executeJS(function(window) {
 
-                  window.addEventListener('load', function() {
+                  window.addEventListener('styleguide:onRendered', function() {
                       window.doTake = true;
                   });
 
               })
               actions.waitForJSCondition(function(window) {
                   return window.doTake;
-              }, 3000);*/
-              actions.wait(3000)
+              }, 7000);
           });
   });
 
