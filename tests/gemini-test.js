3413d931e6920f421993334c4bad0703e2ea0d83
Varya Stepanova <mail@varya.me>
2015-07-24 13:07:13 +0300

    Unbreakable taking screenshots

--- tests/gemini-test.js
+++ tests/gemini-test.js
@@ -1,27 +1,37 @@
 var gemini = require('gemini');
 
 var examples = [
-  '1.1',
   '1.2-1',
+  '1.1',
   '2.1'
 ];
 
+var pages = [];
+pages.push({
+  'name': 'index',
+  url: '/styleguide/#'
+});
 examples.forEach(function(example) {
+  pages.push({
+    'name': example,
+    url: '/styleguide/#/section/' + example + '/fullscreen'
+  });
+});
 
-  gemini.suite(example, function(suite) {
-      suite.setUrl('/styleguide/#/section/' + example + '/fullscreen')
+pages.forEach(function(page) {
+
+  gemini.suite(page.name, function(suite) {
+      if (page.name === 'index') {
+        suite.skip();
+      }
+      suite.setUrl(page.url)
           .setCaptureElements('body')
           .capture('plain', function(actions, find){
-              actions.executeJS(function(window) {
 
-                  window.addEventListener('styleguide:onRendered', function() {
-                      window.doTake = true;
-                  });
+              actions.waitForElementToShow('shadow-dom', 7000);
+              actions.wait(500);
 
-              })
-              actions.waitForJSCondition(function(window) {
-                  return window.doTake;
-              }, 7000);
+
           });
   });
 
