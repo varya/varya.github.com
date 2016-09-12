2e3d3bc942c807387426c2b887ed15244c38ed12
Varya Stepanova <mail@varya.me>
2016-09-12 12:00:27 +0300

    Capture only center of the header

--- tests/visual/config/test_2.1_custom.js
+++ tests/visual/config/test_2.1_custom.js
@@ -3,9 +3,9 @@
 module.exports = function (page) {
   gemini.suite(page.name, function (suite) {
 
-    suite.ignoreElements('.header__left', '.header__right');
+//    suite.ignoreElements('.header__left', '.header__right');
 
-    suite.setUrl(page.url).setCaptureElements('body').capture('plain', function (actions, find) {
+    suite.setUrl(page.url).setCaptureElements('.header__center').capture('plain', function (actions, find) {
 
       actions.waitForElementToShow('shadow-dom', 7000);
       actions.wait(500);
