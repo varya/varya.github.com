18187c248642c914327fc6ce131066f8d9e138a6
Varya Stepanova <mail@varya.me>
2018-07-19 14:43:28 +0300

    Add Yandex.Metrika

--- src/html.js
+++ src/html.js
@@ -10,6 +10,10 @@ if (process.env.NODE_ENV === `production`) {
   }
 }
 
+const metrika = `
+    <!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter20478625 = new Ya.Metrika({id:20478625, clickmap:true, trackLinks:true, accurateTrackBounce:true, trackHash:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/20478625" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
+`;
+
 module.exports = class HTML extends React.Component {
   render() {
     let css;
@@ -45,6 +49,7 @@ module.exports = class HTML extends React.Component {
           {this.props.preBodyComponents}
           <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
           {this.props.postBodyComponents}
+          <div dangerouslySetInnerHTML={{ __html: metrika }} />
         </body>
       </html>
     );
