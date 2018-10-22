364f49a4cf62216f0f6ebc06cfbe0a6afdc4aabb
Varya Stepanova <mail@varya.me>
2018-10-22 15:28:36 +0300

    Do not generate source maps

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,7 @@ module.exports = {
     }
   },
   plugins: [
+    `gatsby-plugin-no-sourcemaps`,
     `gatsby-plugin-react-next`,
     // `gatsby-plugin-styled-jsx`, // the plugin's code is inserted directly to gatsby-node.js and gatsby-ssr.js files
     // 'gatsby-plugin-styled-jsx-postcss', // as above
