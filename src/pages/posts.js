acceef22c2e9fa2c4fb495216d6b6d691c6d2175
Varya Stepanova <mail@varya.me>
2019-01-26 11:24:17 +0200

    Import graphql from Gatsby

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
 import React from "react";
 
+import { graphql } from 'gatsby';
+
 import Blog from "../components/Blog";
 
 class PostsPage extends React.Component {
