0b4a85d60ea8f30b826e15b5500058e976a6890a
Varya Stepanova <mail@varya.me>
2019-05-07 19:11:25 +0300

    Render SVG from src folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -230,7 +230,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-react-svg",
       options: {
-        include: /svg-icons/
+        include: /src/
       }
     },
     'gatsby-plugin-styled-components'
