cc6055b59437d3c03f82b65639591595cf880db5
Varya Stepanova <mail@varya.me>
2019-05-08 21:00:14 +0300

    Colored icons

--- src/components/ProjectRoles/ProjectRoles.js
+++ src/components/ProjectRoles/ProjectRoles.js
@@ -4,10 +4,19 @@ import PropTypes from "prop-types";
 import styled from 'styled-components';
 import breakpoint from 'styled-components-breakpoint'
 
+import { colorScheme } from '../Colors/Colors.js';
+
 import Developer from './developer.svg';
 import Designer from './designer.svg';
 import Owner from './owner.svg';
 import Business from './business.svg';
+
+const Container = styled.div`
+  width: 150px;
+  background-color: ${props => props.color};
+  padding: 15px;
+  border-radius: 50%;
+`;
 
 class ProjectRoles extends React.Component {
 
@@ -20,7 +29,13 @@ class ProjectRoles extends React.Component {
         'business': Business,
       }[this.props.role] || Developer;
 
-    return <Icon/>;
+    const color = {
+      'developer': colorScheme.secondary,
+      'designer': colorScheme.highlight,
+      'owner': colorScheme.shadow,
+    }[this.props.role] || colorScheme.primary;
+
+    return <Container color={color}><Icon/></Container>;
   }
 }
 
