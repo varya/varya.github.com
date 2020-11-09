bd67ebbeea9b8e9327abb2d21e713d7a04061aa9
git-directory-deploy <illustrova@gmail.com>
2020-11-09 09:09:35 +0200

    chore: setup grommet

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.js"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-addon-grommet",
+  ],
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,42 @@
-import "antd/dist/antd.css";
+import { addDecorator } from "@storybook/react"; // <- or your storybook framework
+import { withGrommet } from "storybook-addon-grommet";
+import { grommet } from "grommet";
+// import {
+//   black,
+//   light,
+//   materialdark,
+//   materiallight,
+//   metro,
+// } from "grommet-controls";
+
+const myTheme = {
+  global: {
+    colors: {
+      brand: "#228BE6",
+    },
+    font: {
+      family: "Roboto Light",
+      size: "16px",
+      height: "30px",
+    },
+  },
+};
+
+addDecorator(
+  withGrommet({
+    theme: "myTheme",
+    themes: {
+      myTheme,
+      grommet,
+    },
+    boxProps: {
+      align: "start",
+    },
+    grommetProps: {
+      full: true,
+    },
+  })
+);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
