d41cc37a47ecadcf6313412a56258dda4fea6721
Irina Illustrova <illustrova@gmail.com>
2020-11-20 03:14:40 +0200

    feat: Design Systems page draft

--- src/components/--Pagination/Pagination.js
+++ src/components/--Pagination/Pagination.js
@@ -37,7 +37,7 @@ const paginationTheme = deepMerge(theme, {
       },
     },
     hover: {
-      background: { color: "accent-2" },
+      background: { color: "accent-4" },
       color: "text-weak",
     },
   },
