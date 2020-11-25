738e20a0a3591495a587bc8f6c964ebf2243c0b6
Irina Illustrova <illustrova@gmail.com>
2020-11-25 03:56:43 +0200

    wip: draft of post template

--- src/components/Typography/Typography.js
+++ src/components/Typography/Typography.js
@@ -1,6 +1,6 @@
-import { injectGlobal } from "styled-components";
+// import { injectGlobal } from "styled-components";
 
-import { colorScheme } from "../Colors/Colors.js";
+// import { colorScheme } from "../Colors/Colors.js";
 
 /*
 import LatoBoldEot from './fonts/lato-bold.eot';
@@ -28,168 +28,166 @@ import LatoItalicWoff from './fonts/lato-italic.woff';
 import LatoItalicWoff2 from './fonts/lato-italic.woff2';
 */
 
-injectGlobal`
-/* cyrillic-ext */
-@font-face {
-  font-family: 'Tinos';
-  font-style: normal;
-  font-weight: 400;
-  src: local('Tinos Regular'), local('Tinos-Regular'), url(https://fonts.gstatic.com/s/tinos/v11/buE4poGnedXvwjX2fmRD8iI_wNU.woff2) format('woff2');
-  unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
-}
-/* cyrillic */
-@font-face {
-  font-family: 'Tinos';
-  font-style: normal;
-  font-weight: 400;
-  src: local('Tinos Regular'), local('Tinos-Regular'), url(https://fonts.gstatic.com/s/tinos/v11/buE4poGnedXvwjX_fmRD8iI_wNU.woff2) format('woff2');
-  unicode-range: U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;
-}
-/* latin-ext */
-@font-face {
-  font-family: 'Tinos';
-  font-style: normal;
-  font-weight: 400;
-  src: local('Tinos Regular'), local('Tinos-Regular'), url(https://fonts.gstatic.com/s/tinos/v11/buE4poGnedXvwjX1fmRD8iI_wNU.woff2) format('woff2');
-  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-}
-/* latin */
-@font-face {
-  font-family: 'Tinos';
-  font-style: normal;
-  font-weight: 400;
-  src: local('Tinos Regular'), local('Tinos-Regular'), url(https://fonts.gstatic.com/s/tinos/v11/buE4poGnedXvwjX7fmRD8iI_.woff2) format('woff2');
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-}
+// injectGlobal`
+// /* cyrillic-ext */
+// @font-face {
+//   font-family: 'Tinos';
+//   font-style: normal;
+//   font-weight: 400;
+//   src: local('Tinos Regular'), local('Tinos-Regular'), url(https://fonts.gstatic.com/s/tinos/v11/buE4poGnedXvwjX2fmRD8iI_wNU.woff2) format('woff2');
+//   unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
+// }
+// /* cyrillic */
+// @font-face {
+//   font-family: 'Tinos';
+//   font-style: normal;
+//   font-weight: 400;
+//   src: local('Tinos Regular'), local('Tinos-Regular'), url(https://fonts.gstatic.com/s/tinos/v11/buE4poGnedXvwjX_fmRD8iI_wNU.woff2) format('woff2');
+//   unicode-range: U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;
+// }
+// /* latin-ext */
+// @font-face {
+//   font-family: 'Tinos';
+//   font-style: normal;
+//   font-weight: 400;
+//   src: local('Tinos Regular'), local('Tinos-Regular'), url(https://fonts.gstatic.com/s/tinos/v11/buE4poGnedXvwjX1fmRD8iI_wNU.woff2) format('woff2');
+//   unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+// }
+// /* latin */
+// @font-face {
+//   font-family: 'Tinos';
+//   font-style: normal;
+//   font-weight: 400;
+//   src: local('Tinos Regular'), local('Tinos-Regular'), url(https://fonts.gstatic.com/s/tinos/v11/buE4poGnedXvwjX7fmRD8iI_.woff2) format('woff2');
+//   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+// }
 
+// /* latin-ext */
+// @font-face {
+//   font-family: 'Lato';
+//   font-style: normal;
+//   font-weight: 400;
+//   src: local('Lato Regular'), local('Lato-Regular'), url(https://fonts.gstatic.com/s/lato/v14/S6uyw4BMUTPHjxAwXiWtFCfQ7A.woff2) format('woff2');
+//   unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+// }
+// /* latin */
+// @font-face {
+//   font-family: 'Lato';
+//   font-style: normal;
+//   font-weight: 400;
+//   src: local('Lato Regular'), local('Lato-Regular'), url(https://fonts.gstatic.com/s/lato/v14/S6uyw4BMUTPHjx4wXiWtFCc.woff2) format('woff2');
+//   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+// }
+// /* latin-ext */
+// @font-face {
+//   font-family: 'Lato';
+//   font-style: normal;
+//   font-weight: 700;
+//   src: local('Lato Bold'), local('Lato-Bold'), url(https://fonts.gstatic.com/s/lato/v14/S6u9w4BMUTPHh6UVSwaPGQ3q5d0N7w.woff2) format('woff2');
+//   unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+// }
+// /* latin */
+// @font-face {
+//   font-family: 'Lato';
+//   font-style: normal;
+//   font-weight: 700;
+//   src: local('Lato Bold'), local('Lato-Bold'), url(https://fonts.gstatic.com/s/lato/v14/S6u9w4BMUTPHh6UVSwiPGQ3q5d0.woff2) format('woff2');
+//   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+// }
+// `;
 
+// injectGlobal`
+// /* TODO: load from website for cyrillic pages only */
 
-/* latin-ext */
-@font-face {
-  font-family: 'Lato';
-  font-style: normal;
-  font-weight: 400;
-  src: local('Lato Regular'), local('Lato-Regular'), url(https://fonts.gstatic.com/s/lato/v14/S6uyw4BMUTPHjxAwXiWtFCfQ7A.woff2) format('woff2');
-  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-}
-/* latin */
-@font-face {
-  font-family: 'Lato';
-  font-style: normal;
-  font-weight: 400;
-  src: local('Lato Regular'), local('Lato-Regular'), url(https://fonts.gstatic.com/s/lato/v14/S6uyw4BMUTPHjx4wXiWtFCc.woff2) format('woff2');
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-}
-/* latin-ext */
-@font-face {
-  font-family: 'Lato';
-  font-style: normal;
-  font-weight: 700;
-  src: local('Lato Bold'), local('Lato-Bold'), url(https://fonts.gstatic.com/s/lato/v14/S6u9w4BMUTPHh6UVSwaPGQ3q5d0N7w.woff2) format('woff2');
-  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-}
-/* latin */
-@font-face {
-  font-family: 'Lato';
-  font-style: normal;
-  font-weight: 700;
-  src: local('Lato Bold'), local('Lato-Bold'), url(https://fonts.gstatic.com/s/lato/v14/S6u9w4BMUTPHh6UVSwiPGQ3q5d0.woff2) format('woff2');
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-}
-`;
+// @font-face {
+//   font-family: 'Lato';
+//   src: url("/static/fonts/lato-bold.eot");
+//   src: local("Lato Bold"), local("Lato-Bold"), url("/static/fonts/lato-bold.eot") format("embedded-opentype"), url("/static/fonts/lato-bold.woff2") format("woff2"), url("/static/fonts/lato-bold.woff") format("woff"), url("/static/fonts/lato-bold.ttf") format("truetype"), url("/static/fonts/lato-bold.svg#lato") format("svg");
+//   font-weight: 700;
+//   font-style: normal; }
+// @font-face {
+//   font-family: 'Lato';
+//   src: url("/static/fonts/lato-bolditalic.eot");
+//   src: local("Lato Bold Italic"), local("Lato-BoldItalic"), url("/static/fonts/lato-bolditalic.eot") format("embedded-opentype"), url("/static/fonts/lato-bolditalic.woff2") format("woff2"), url("/static/fonts/lato-bolditalic.woff") format("woff"), url("/static/fonts/lato-bolditalic.ttf") format("truetype"), url("/static/fonts/lato-bolditalic.svg#lato") format("svg");
+//   font-weight: 700;
+//   font-style: italic; }
+// @font-face {
+//   font-family: 'Lato';
+//   src: url("/static/fonts/lato-regular.eot");
+//   src: local("Lato Regular"), local("Lato-Regular"), url("/static/fonts/lato-regular.eot") format("embedded-opentype"), url("/static/fonts/lato-regular.woff2") format("woff2"), url("/static/fonts/lato-regular.woff") format("woff"), url("/static/fonts/lato-regular.ttf") format("truetype"), url("/static/fonts/lato-regular.svg#lato") format("svg");
+//   font-weight: 400;
+//   font-style: normal; }
+// @font-face {
+//   font-family: 'Lato';
+//   src: url("/static/fonts/lato-italic.eot");
+//   src: local("Lato Italic"), local("Lato-Italic"), url("/static/fonts/lato-italic.eot") format("embedded-opentype"), url("/static/fonts/lato-italic.woff2") format("woff2"), url("/static/fonts/lato-italic.woff") format("woff"), url("/static/fonts/lato-italic.ttf") format("truetype"), url("/static/fonts/lato-italic.svg#lato") format("svg");
+//   font-weight: 400;
+//   font-style: italic; }
+// `;
 
-injectGlobal`
-/* TODO: load from website for cyrillic pages only */
+// injectGlobal`
+// * {
+//   font-family: 'Lato', 'Helvetica Neue', Helvetica;
+//   color: ${colorScheme.dark};
+// }
 
-@font-face {
-  font-family: 'Lato';
-  src: url("/static/fonts/lato-bold.eot");
-  src: local("Lato Bold"), local("Lato-Bold"), url("/static/fonts/lato-bold.eot") format("embedded-opentype"), url("/static/fonts/lato-bold.woff2") format("woff2"), url("/static/fonts/lato-bold.woff") format("woff"), url("/static/fonts/lato-bold.ttf") format("truetype"), url("/static/fonts/lato-bold.svg#lato") format("svg");
-  font-weight: 700;
-  font-style: normal; }
-@font-face {
-  font-family: 'Lato';
-  src: url("/static/fonts/lato-bolditalic.eot");
-  src: local("Lato Bold Italic"), local("Lato-BoldItalic"), url("/static/fonts/lato-bolditalic.eot") format("embedded-opentype"), url("/static/fonts/lato-bolditalic.woff2") format("woff2"), url("/static/fonts/lato-bolditalic.woff") format("woff"), url("/static/fonts/lato-bolditalic.ttf") format("truetype"), url("/static/fonts/lato-bolditalic.svg#lato") format("svg");
-  font-weight: 700;
-  font-style: italic; }
-@font-face {
-  font-family: 'Lato';
-  src: url("/static/fonts/lato-regular.eot");
-  src: local("Lato Regular"), local("Lato-Regular"), url("/static/fonts/lato-regular.eot") format("embedded-opentype"), url("/static/fonts/lato-regular.woff2") format("woff2"), url("/static/fonts/lato-regular.woff") format("woff"), url("/static/fonts/lato-regular.ttf") format("truetype"), url("/static/fonts/lato-regular.svg#lato") format("svg");
-  font-weight: 400;
-  font-style: normal; }
-@font-face {
-  font-family: 'Lato';
-  src: url("/static/fonts/lato-italic.eot");
-  src: local("Lato Italic"), local("Lato-Italic"), url("/static/fonts/lato-italic.eot") format("embedded-opentype"), url("/static/fonts/lato-italic.woff2") format("woff2"), url("/static/fonts/lato-italic.woff") format("woff"), url("/static/fonts/lato-italic.ttf") format("truetype"), url("/static/fonts/lato-italic.svg#lato") format("svg");
-  font-weight: 400;
-  font-style: italic; }
-`;
+// html, body {
+//   font-size: 20px;
+//   line-height: 2em;
+//   font-weight: 400;
+// }
 
-injectGlobal`
-* {
-  font-family: 'Lato', 'Helvetica Neue', Helvetica;
-  color: ${colorScheme.dark};
-}
+// h1 {
+//   font-size: 2.4em;
+//   font-weight: 400;
+//   line-height: 1.25em;
+//   margin-top: 0;
+// }
 
-html, body {
-  font-size: 20px;
-  line-height: 2em;
-  font-weight: 400;
-}
+// h2 {
+//   font-size: 2em;
+//   font-weight: bold;
 
-h1 {
-  font-size: 2.4em;
-  font-weight: 400;
-  line-height: 1.25em;
-  margin-top: 0;
-}
+// }
 
-h2 {
-  font-size: 2em;
-  font-weight: bold;
+// h3 {
+//   font-size: 1.5em;
+//   font-weight: bold;
+// }
 
-}
+// h4 {
+//   font-size: 1.25em;
+//   font-weight: bold;
+// }
+// h5 {
+//   font-size: 1em;
+//   font-weight: bold;
+// }
 
-h3 {
-  font-size: 1.5em;
-  font-weight: bold;
-}
+// h1, h2, h3, h4, h5, h6 {
+//   .anchor {
+//     font-size: 0.75em;
+//     margin-left: -1em;
 
-h4 {
-  font-size: 1.25em;
-  font-weight: bold;
-}
-h5 {
-  font-size: 1em;
-  font-weight: bold;
-}
+//     svg {
+//       width: 0.75em;
+//       height: 0.75em;
+//     }
+//   }
+// }
 
-h1, h2, h3, h4, h5, h6 {
-  .anchor {
-    font-size: 0.75em;
-    margin-left: -1em;
+// a {
+//   color: ${colorScheme.primary}
+//   text-decoration: underline;
+// }
 
-    svg {
-      width: 0.75em;
-      height: 0.75em;
-    }
-  }
-}
+// .clearfix {
+//   &:after {
+//     content: "";
+//     display: table;
+//     clear: both;
+//   }
+// }
+// `;
 
-a {
-  color: ${colorScheme.primary}
-  text-decoration: underline;
-}
-
-.clearfix {
-  &:after {
-    content: "";
-    display: table;
-    clear: both;
-  }
-}
-`;
-
-export default {};
+// export default {};
--- src/templates/post/Post.js
+++ src/templates/post/Post.js
@@ -10,88 +10,122 @@ import Image from "../../components/--Image";
 import PrevNextNav from "../../components/--PrevNextNav";
 import Img1 from "../../../content/posts/webfonts-with-sass-and-webpack/thumb.png";
 import Img2 from "../../../content/posts/webfonts-with-sass-and-webpack/enjoy.png";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
-const Post = ({ imageUrl, tags, date, readingTime, title }) => (
-  <Layout>
-    <PostHeader
-      imageUrl={imageUrl}
-      tags={tags}
-      date={date}
-      readingTime={readingTime}
-      title={title}
-    />
-    <Box
-      flex="grow"
-      width="xlarge"
-      margin={{ horizontal: "auto" }}
-      pad="medium"
-      direction="column"
-    >
-      <Paragraph lead>
-        Bacon ipsum dolor amet chislic filet mignon cow, spare ribs short loin
-        beef ribs pork chop. Tail frankfurter ribeye pork chop pig rump short
-        ribs pork bresaola tongue shoulder jerky alcatra jowl. Boudin swine
-        filet mignon, fatback kielbasa leberkas pork belly pig salami alcatra
-        doner prosciutto t-bone. Ham bacon prosciutto, pork belly turkey tri-tip
-        pancetta. Alcatra tenderloin t-bone buffalo beef ribs fatback burgdoggen
-        spare ribs pork loin pork chop salami capicola hamburger pork drumstick.
-        Drumstick jerky jowl, short ribs chislic turducken burgdoggen kevin
-        venison shoulder pancetta cupim tongue meatball ham.
-      </Paragraph>
+const Post = ({
+  data: {
+    mdx,
+    site: { siteMetadata },
+  },
+  pageContext: { next, prev, fileSourceUrl },
+  location,
+}) => {
+  const { tags, date, readingTime } = mdx.fields;
+  console.log(
+    "🚀 ~ file: Post.js ~ line 24 ~ tags, date, readingTime",
+    tags,
+    date,
+    readingTime
+  );
+  const { title, subTitle, cover } = mdx.frontmatter;
+  console.log(
+    "🚀 ~ file: Post.js ~ line 31 ~ mdx.frontmatter",
+    mdx.frontmatter
+  );
+  console.log(
+    "🚀 ~ file: Post.js ~ line 26 ~ title, subTitle, cover",
+    title,
+    subTitle,
+    cover
+  );
 
-      <Box flex={false}>
-        <Image
-          src={Img1}
-          caption="test caption"
-          copyright={{ text: "Jane Doe" }}
+  // pageContext: { imageUrl, tags = [], date, readingTime, title },
+  return (
+    <Layout>
+      <PostHeader
+        // imageUrl={cover}
+        tags={tags}
+        date={date}
+        // readingTime={`${Math.round(readingTime.minutes)} min`}
+        title={title}
+      />
+      <Box
+        flex="grow"
+        width="xlarge"
+        margin={{ horizontal: "auto" }}
+        pad="medium"
+        direction="column"
+      >
+        <MDXRenderer>{mdx.body}</MDXRenderer>
+        <Paragraph lead>
+          Bacon ipsum dolor amet chislic filet mignon cow, spare ribs short loin
+          beef ribs pork chop. Tail frankfurter ribeye pork chop pig rump short
+          ribs pork bresaola tongue shoulder jerky alcatra jowl. Boudin swine
+          filet mignon, fatback kielbasa leberkas pork belly pig salami alcatra
+          doner prosciutto t-bone. Ham bacon prosciutto, pork belly turkey
+          tri-tip pancetta. Alcatra tenderloin t-bone buffalo beef ribs fatback
+          burgdoggen spare ribs pork loin pork chop salami capicola hamburger
+          pork drumstick. Drumstick jerky jowl, short ribs chislic turducken
+          burgdoggen kevin venison shoulder pancetta cupim tongue meatball ham.
+        </Paragraph>
+
+        <Box flex={false}>
+          <Image
+            src={Img1}
+            caption="test caption"
+            copyright={{ text: "Jane Doe" }}
+          />
+        </Box>
+        <Paragraph>
+          Turkey t-bone pork belly ball tip alcatra pork chop. Capicola meatloaf
+          short loin burgdoggen ball tip, kevin andouille meatball biltong
+          boudin landjaeger shoulder corned beef buffalo. Pork strip steak
+          hamburger, sausage jerky pork chop ham drumstick kielbasa sirloin.
+          Brisket pork chop rump fatback, jowl ball tip jerky swine boudin
+          biltong. Chuck spare ribs strip steak, flank jerky ground round
+          prosciutto pork loin rump capicola drumstick tail. Ground round
+          porchetta sirloin jerky flank.
+        </Paragraph>
+        <Paragraph fill>
+          Brisket jowl ground round drumstick ribeye corned beef pork chop
+          tongue meatloaf beef ribs biltong rump buffalo. Porchetta pork
+          leberkas, ham landjaeger boudin filet mignon short ribs shankle
+          pastrami cow beef ribs ribeye jowl sirloin. Chicken swine picanha
+          doner ball tip strip steak sirloin ham hock tail chislic jowl t-bone
+          alcatra. Doner ham hock short ribs, salami pork chop flank turkey ham
+          ribeye frankfurter kielbasa. Cupim beef andouille picanha chislic,
+          shoulder filet mignon sirloin shankle frankfurter.
+        </Paragraph>
+        <Box flex={false}>
+          <Image src={Img2} fit="contain" />
+        </Box>
+        <Box
+          direction="row"
+          fill="horizontal"
+          justify="center"
+          margin={{ bottom: "auto" }}
+          wrap="true"
+          pad={{ vertical: "medium" }}
+        >
+          {tags &&
+            tags.length > 0 &&
+            tags.map((tag) => <Tag key={tag} name={tag} margin="xsmall" />)}
+        </Box>
+        <PrevNextNav
+          flex={false}
+          prevSlug="#"
+          nextSlug="#"
+          prevTitle="Remote work in a design system team"
+          nextTitle="Flatten array with Javascript reduce function"
+          pad={{ vertical: "medium" }}
         />
       </Box>
-      <Paragraph>
-        Turkey t-bone pork belly ball tip alcatra pork chop. Capicola meatloaf
-        short loin burgdoggen ball tip, kevin andouille meatball biltong boudin
-        landjaeger shoulder corned beef buffalo. Pork strip steak hamburger,
-        sausage jerky pork chop ham drumstick kielbasa sirloin. Brisket pork
-        chop rump fatback, jowl ball tip jerky swine boudin biltong. Chuck spare
-        ribs strip steak, flank jerky ground round prosciutto pork loin rump
-        capicola drumstick tail. Ground round porchetta sirloin jerky flank.
-      </Paragraph>
-      <Paragraph fill>
-        Brisket jowl ground round drumstick ribeye corned beef pork chop tongue
-        meatloaf beef ribs biltong rump buffalo. Porchetta pork leberkas, ham
-        landjaeger boudin filet mignon short ribs shankle pastrami cow beef ribs
-        ribeye jowl sirloin. Chicken swine picanha doner ball tip strip steak
-        sirloin ham hock tail chislic jowl t-bone alcatra. Doner ham hock short
-        ribs, salami pork chop flank turkey ham ribeye frankfurter kielbasa.
-        Cupim beef andouille picanha chislic, shoulder filet mignon sirloin
-        shankle frankfurter.
-      </Paragraph>
-      <Box flex={false}>
-        <Image src={Img2} fit="contain" />
-      </Box>
-      <Box
-        direction="row"
-        fill="horizontal"
-        justify="center"
-        margin={{ bottom: "auto" }}
-        wrap="true"
-        pad={{ vertical: "medium" }}
-      >
-        {tags.length > 0 &&
-          tags.map((tag) => <Tag key={tag} name={tag} margin="xsmall" />)}
-      </Box>
-      <PrevNextNav
-        flex={false}
-        prevSlug="#"
-        nextSlug="#"
-        prevTitle="Remote work in a design system team"
-        nextTitle="Flatten array with Javascript reduce function"
-        pad={{ vertical: "medium" }}
-      />
-    </Box>
-  </Layout>
-);
-
+    </Layout>
+  );
+};
 Post.propTypes = {
+  data: PropTypes.object,
+  location: PropTypes.object,
   imageUrl: PropTypes.string,
   tags: PropTypes.array,
   date: PropTypes.string,
@@ -100,3 +134,43 @@ Post.propTypes = {
 };
 
 export default Post;
+
+export const pageQuery = graphql`
+  query PostBySlug2($slug: String!) {
+    mdx(fields: { slug: { eq: $slug } }) {
+      id
+      body
+      fields {
+        readingTime {
+          minutes
+        }
+        slug
+        prefix
+        disqusIdentifier
+      }
+      frontmatter {
+        title
+        subTitle
+        date(formatString: "DD MMMM YYYY")
+        v2
+        old
+        tumblr
+        meta {
+          desc
+        }
+        cover {
+          childImageSharp {
+            resize(width: 300) {
+              src
+            }
+          }
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        siteUrl
+      }
+    }
+  }
+`;
