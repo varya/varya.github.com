b4b3804d6ce021037f3443df11a7d7207e0f79dc
Irina Illustrova <illustrova@gmail.com>
2020-11-27 01:03:37 +0200

    feat: adjust Image to work with mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 800,
+              maxWidth: 1600,
               backgroundColor: "transparent",
             },
           },
