abe9e5ddc2f09b812d5d1a4f9e598cef3cdcc49d
Irina Illustrova <illustrova@gmail.com>
2020-12-04 02:20:01 +0200

    content: add supervision page

---
+++ src/pages/services/supervision.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+import Page from "../../templates/page/Page";
+import Paragraph from "../../components/--Paragraph";
+import Heading from "../../components/--Heading";
+import { Text } from "grommet";
+
+import heroImage from "./hero-supervision.jpg";
+
+const HeroContent = () => (
+  <Heading
+    level={1}
+    alignSelf="center"
+    responsive
+    size="large"
+    margin={{ top: "auto" }}
+  >
+    <Text size="inherit" color="accent">
+      Supervising{" "}
+    </Text>
+    <Text size="inherit" color="brand">
+      the teams
+    </Text>
+  </Heading>
+);
+
+const Supervision = () => (
+  <Page
+    hero={{
+      props: {
+        imageUrl: heroImage,
+        hasOverlay: true,
+      },
+      content: HeroContent,
+    }}
+  >
+    <Paragraph>
+      As an experienced team leader, I can offer my help in supervising the
+      teams of designers and developers.
+    </Paragraph>
+    <Paragraph>
+      If you do not have your own specialists but are interested in getting a
+      bunch of professionals working together, my partners and I can offer a
+      well-worked team proven itself at many other projects.
+    </Paragraph>
+  </Page>
+);
+
+export default Supervision;
