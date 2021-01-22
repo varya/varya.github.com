ed30b111f474e7b77c34dbd79c7ed3d542e882a9
Varya Stepanova <mail@varya.me>
2021-01-22 18:35:11 +0200

    Use Google Analytics via plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,6 +83,17 @@ module.exports = {
     `gatsby-plugin-catch-links`,
     `gatsby-remark-reading-time`,
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-VNR46J539J", // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: config.manifestName,
--- src/html.js
+++ src/html.js
@@ -12,31 +12,6 @@ if (process.env.NODE_ENV === `production`) {
   }
 }
 
-// Old analytics tag
-// const googleAnalytics = `
-//     <!-- Global site tag (gtag.js) - Google Analytics -->
-// <script async src="https://www.googletagmanager.com/gtag/js?id=UA-128056453-1"></script>
-// <script>
-//   window.dataLayer = window.dataLayer || [];
-//   function gtag(){dataLayer.push(arguments);}
-//   gtag('js', new Date());
-
-//   gtag('config', 'UA-128056453-1');
-// </script>
-// `;
-
-const googleAnalytics = `
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-VNR46J539J"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-VNR46J539J');
-</script>
-`;
-
 export default function HTML(props) {
   let css;
   if (process.env.NODE_ENV === `production`) {
@@ -136,7 +111,6 @@ export default function HTML(props) {
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
         {props.postBodyComponents}
-        <div dangerouslySetInnerHTML={{ __html: googleAnalytics }} />
       </body>
     </html>
   );
